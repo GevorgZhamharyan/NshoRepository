d074601075ed60a7cfd8443a584938ee1047b27b
Gevorg Zhamharyan <gevorgzhamharyan@outlook.com>
2023-05-13 14:35:52 +0400

    I'm crossed

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace NshanApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Nsh!");
-            Console.WriteLine("Austria"); 
+            Console.WriteLine("Austria");
+
+            Console.WriteLine("Nshan is my best friend!"); 
         }
     }
 }
